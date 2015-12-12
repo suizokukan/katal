c2e989e1594fa3dc6f2146d3ebae0dc0aac81e08
suizokukan <suizokukan@orange.fr>
2015-12-12 20:31:35 +0100

    updated create_faked_target.sh

--- create_faked_target.sh
+++ create_faked_target.sh
@@ -19,43 +19,43 @@ cd faked/target
 echo "(07) ../../katal/katal.py --add --move --strictcmp"
 ../../katal/katal.py --add --move --strictcmp
 
-# echo "(08) ../../katal/katal.py --addtag=goose --to=*5.py"
-# ../../katal/katal.py --addtag=goose --to=*5.py
+echo "(08) ../../katal/katal.py --addtag=goose --to=*5.py"
+../../katal/katal.py --addtag=goose --to=*5.py
 
-# echo "(09) ../../katal/katal.py --addtag=tree --to=*9.py"
-# ../../katal/katal.py --addtag=tree --to=*9.py
+echo "(09) ../../katal/katal.py --addtag=tree --to=*9.py"
+../../katal/katal.py --addtag=tree --to=*9.py
 
-# echo "(10) ../../katal/katal.py --addtag=pink --to=*1*.py"
-# ../../katal/katal.py --addtag=pink --to=*1*.py
+echo "(10) ../../katal/katal.py --addtag=pink --to=*1*.py"
+../../katal/katal.py --addtag=pink --to=*1*.py
 
-# echo "(11) ../../katal/katal.py --rmnotag"
-# ../../katal/katal.py --rmnotag
+echo "(11) ../../katal/katal.py --rmnotag"
+../../katal/katal.py --rmnotag
 
-# echo "(12) rm 12.py"
-# rm 12.py
+echo "(12) rm 12.py"
+rm 12.py
 
-# echo "(13) rm 14.py"
-# rm 14.py
+echo "(13) rm 14.py"
+rm 14.py
 
-# echo "(14) ../../katal/katal.py --cleandbrm"
-# ../../katal/katal.py --cleandbrm
+echo "(14) ../../katal/katal.py --cleandbrm"
+../../katal/katal.py --cleandbrm
 
-# echo "(15) ../../katal/katal.py --rmtags --to=10.py"
-# ../../katal/katal.py --rmtags --to=10.py
+echo "(15) ../../katal/katal.py --rmtags --to=10.py"
+../../katal/katal.py --rmtags --to=10.py
 
-# echo "(16) ../../katal/katal.py --findtag=goose"
-# ../../katal/katal.py --findtag=goose
+echo "(16) ../../katal/katal.py --findtag=goose"
+../../katal/katal.py --findtag=goose
 
-# echo "(17) ../../katal/katal.py --settagsstr="red;marble" --to=13.py"
-# ../../katal/katal.py --settagsstr="red;marble" --to=13.py
+echo "(17) ../../katal/katal.py --settagsstr="red;marble" --to=13.py"
+../../katal/katal.py --settagsstr="red;marble" --to=13.py
 
-# echo "(18) ../../katal/katal.py --findtag=pink --copyto=xyz"
-# ../../katal/katal.py --findtag=pink --copyto=xyz
+echo "(18) ../../katal/katal.py --findtag=pink --copyto=xyz"
+../../katal/katal.py --findtag=pink --copyto=xyz
 
-# echo "(19) ../../katal/katal.py -ti"
-# ../../katal/katal.py -ti
+echo "(19) ../../katal/katal.py -ti"
+../../katal/katal.py -ti
 
-# echo "(20) ../../katal/katal.py --whatabout 0.py"
-# ../../katal/katal.py --whatabout 0.py
+echo "(20) ../../katal/katal.py --whatabout 0.py"
+../../katal/katal.py --whatabout 0.py
 
 
