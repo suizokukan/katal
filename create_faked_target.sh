dfe496f5bcf922ca48ed2fcaa8ae31c566c51457
suizokukan <suizokukan@orange.fr>
2016-01-14 00:38:56 +0100

    fixed create_faked_target.sh : added the --configfile option in order to preempt an eventual cfg file in ~/.katal

--- create_faked_target.sh
+++ create_faked_target.sh
@@ -1,40 +1,49 @@
+# NB : the --configfile option is given in order to preempt an eventual
+#      configuration file in ~/.katal/katal.ini  .
+
+echo
 echo "(01) rm -rf faked/target"
 rm -rf faked/target
 
+echo
 echo "(02) rm -rf faked/src2"
 rm -rf faked/src2
 
+echo
 echo "(03) cp -r faked/src faked/src2"
 cp -r faked/src faked/src2
 
+echo
 echo "(04) ./katal/katal.py --new=faked/target --mute"
 ./katal/katal.py --new=faked/target --mute
 
+echo
 echo "(05) cp faked/katal.ini faked/target/.katal"
 cp faked/katal.ini faked/target/.katal
 
+echo
 echo "(06) cd faked/target"
 cd faked/target
 
 echo
-echo "(07) ../../katal/katal.py --add --move --strictcmp"
-../../katal/katal.py --add --move --strictcmp
+echo "(07) ../../katal/katal.py --add --move --strictcmp --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --add --move --strictcmp --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(08) ../../katal/katal.py --addtag=goose --to=*5.py"
-../../katal/katal.py --addtag=goose --to=*5.py
+echo "(08) ../../katal/katal.py --addtag=goose --to=*5.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --addtag=goose --to=*5.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(09) ../../katal/katal.py --addtag=tree --to=*9.py"
-../../katal/katal.py --addtag=tree --to=*9.py
+echo "(09) ../../katal/katal.py --addtag=tree --to=*9.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --addtag=tree --to=*9.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(10) ../../katal/katal.py --addtag=pink --to=*1*.py"
-../../katal/katal.py --addtag=pink --to=*1*.py
+echo "(10) ../../katal/katal.py --addtag=pink --to=*1*.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --addtag=pink --to=*1*.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(11) ../../katal/katal.py --rmnotag"
-../../katal/katal.py --rmnotag
+echo "(11) ../../katal/katal.py --rmnotag --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --rmnotag --configfile=../../faked/target/.katal/katal.ini
 
 echo
 echo "(12) rm 12.py"
@@ -45,33 +54,33 @@ echo "(13) rm 14.py"
 rm 14.py
 
 echo
-echo "(14) ../../katal/katal.py --cleandbrm"
-../../katal/katal.py --cleandbrm
+echo "(14) ../../katal/katal.py --cleandbrm --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --cleandbrm --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(15) ../../katal/katal.py --rmtags --to=10.py"
-../../katal/katal.py --rmtags --to=10.py
+echo "(15) ../../katal/katal.py --rmtags --to=10.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --rmtags --to=10.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(16) ../../katal/katal.py --findtag=goose"
-../../katal/katal.py --findtag=goose
+echo "(16) ../../katal/katal.py --findtag=goose --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --findtag=goose --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(17) ../../katal/katal.py --settagsstr="red;marble" --to=13.py"
-../../katal/katal.py --settagsstr="red;marble" --to=13.py
+echo "(17) ../../katal/katal.py --settagsstr="red;marble" --to=13.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --settagsstr="red;marble" --to=13.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(18) ../../katal/katal.py --findtag=pink --copyto=xyz"
-../../katal/katal.py --findtag=pink --copyto=xyz
+echo "(18) ../../katal/katal.py --findtag=pink --copyto=xyz --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --findtag=pink --copyto=xyz --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(19) ../../katal/katal.py -ti"
-../../katal/katal.py -ti
+echo "(19) ../../katal/katal.py -ti --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py -ti --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(20) ../../katal/katal.py --whatabout 0.py"
-../../katal/katal.py --whatabout 0.py
+echo "(20) ../../katal/katal.py --whatabout 0.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --whatabout 0.py --configfile=../../faked/target/.katal/katal.ini
 
 echo
-echo "(20) ../../katal/katal.py --whatabout ../src/2.py"
-../../katal/katal.py --whatabout ../src/game.py
+echo "(20) ../../katal/katal.py --whatabout ../src/2.py --configfile=../../faked/target/.katal/katal.ini"
+../../katal/katal.py --whatabout ../src/game.py --configfile=../../faked/target/.katal/katal.ini
