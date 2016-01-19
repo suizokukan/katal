d2a8b0214d3a553be245805b6178a9551dad2536
suizokukan <suizokukan@orange.fr>
2016-01-19 17:45:32 +0100

    bumped version number 0.3a; fixed a typo in roadmap.txt

--- katal/katal.py
+++ katal/katal.py
@@ -65,7 +65,7 @@ import unicodedata
 #
 #===============================================================================
 __projectname__ = "Katal"
-__version__ = "0.2.9"
+__version__ = "0.3a"
 __laststableversion__ = "0.2.9"
 __author__ = "Xavier Faure (suizokukan / 94.23.197.37)"
 __copyright__ = "Copyright 2015, suizokukan"
