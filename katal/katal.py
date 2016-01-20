36aa48f2dd102696ba1fb45b4a34d8d26ac79c50
suizokukan <suizokukan@orange.fr>
2016-01-20 19:39:27 +0100

    bumped version number to 0.3

--- katal/katal.py
+++ katal/katal.py
@@ -65,8 +65,8 @@ import unicodedata
 #
 #===============================================================================
 __projectname__ = "Katal"
-__version__ = "0.3a"
-__laststableversion__ = "0.2.9"
+__version__ = "0.3"
+__laststableversion__ = "0.3"
 __author__ = "Xavier Faure (suizokukan / 94.23.197.37)"
 __copyright__ = "Copyright 2015, suizokukan"
 __license__ = "GPL-3.0"
